06e4bd69be465f1323a12678fd3e7e4346f43802
Georgi-A-Andreev <g.andreev9@gmail.com>
2023-04-06 13:06:46 +0300

    Update workshop_with_bonus.py
    made all the validation about input and added more selectable features

--- softuni_courses/python_advanced/workshops/workshop_with_bonus.py
+++ softuni_courses/python_advanced/workshops/workshop_with_bonus.py
@@ -9,7 +9,15 @@ def create_field(row, col):
 
 
 def player_choice(player):
-    return int(input(f'Player {current_player}, please choose a column:\n')) - 1
+    while True:
+        current_pick = int(input(f'Player {player}, please choose a column: ')) - 1
+        try:
+            if not 0 <= current_pick <= cols:
+                raise IndexError
+        except IndexError:
+            print('Invalid position, try again!')
+        else:
+            return current_pick
 
 
 def print_field(matrix, player, choice):
@@ -18,34 +26,36 @@ def print_field(matrix, player, choice):
             matrix[i][choice] = player
             [print(i) for i in matrix]
             break
-    return i, choice
 
 
-def validation(field, player):
-    for r in range(field):
+def validation(field, player, combinations_to_win):
+    lst, lst2, lst3, lst4 = [], [], [], []
+    for r in range(len(field)):
         for c in field[r]:
             if c == player:
-                lst = field[r][field.index(c):min(len(field[r]) - 1, field.index(c) + 4)]
-                if field.index(c) <= len(field) - 4:
-                    lst2 = [field[r + i][field.index(c)] for i in range(len(field) - 4)]
-                if r >= 3 and matrix[r].index(c) <= len(matrix[r]) - 4:
-                    lst3 = [field[r - i][field.index(c) + i] for i in range(len(field) - 4)]
-                if r >= 3 and matrix[r].index(c) >= 3:
-                    lst4 = [field[r - i][field.index(c) - i] for i in range(len(field) - 4)]
-            for i in (lst, lst2, lst3, lst4):
-                if len(i) == 4 and set(i) == 1:
-                    return True
+                lst = field[r][field[r].index(c):min(len(field[r]), field[r].index(c) + combinations_to_win)]
+                if r <= len(field) - combinations_to_win:
+                    lst2 = [field[r + i][field[r].index(c)] for i in range(combinations_to_win)]
+                if r >= combinations_to_win - 1 and matrix[r].index(c) <= len(matrix[r]) - combinations_to_win:
+                    lst3 = [field[r - i][field[r].index(c) + i] for i in range(combinations_to_win)]
+                if r >= combinations_to_win - 1 and matrix[r].index(c) >= combinations_to_win - 1:
+                    lst4 = [field[r - i][field[r].index(c) - i] for i in range(combinations_to_win)]
+                for i in (lst, lst2, lst3, lst4):
+                    if len(i) == combinations_to_win and len(set(i)) == 1:
+                        return 'winner'
 
 
 max_players = deque(range(1, int(input('How many players will play? : ')) + 1))
 rows = int(input('How many rows? : '))
 cols = int(input('How many cols? : '))
+combinations = int(input('How many positions in a row to win? : '))
 matrix = create_field(rows, cols)
-current_player = max_players[0]
-choice = player_choice(current_player)
-print_field(matrix, current_player, choice)
-if validation(matrix, current_player):
-    print(f'The winner is player {current_player}')
-    exit()
 
-max_players.append(max_players.popleft())
+while True:
+    current_player = max_players[0]
+    choice = player_choice(current_player)
+    print_field(matrix, current_player, choice)
+    if validation(matrix, current_player, combinations) == 'winner':
+        print(f'The winner is player {current_player}')
+        break
+    max_players.append(max_players.popleft())
