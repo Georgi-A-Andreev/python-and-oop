794185e3a7f9c2811c31e2b4bd9d85ba42ed7432
Georgi-A-Andreev <g.andreev9@gmail.com>
2023-05-02 22:36:35 +0300

    Gym
    class Trainer

---
+++ softuni_courses/python_oop/class_and_static_methods_exercise/project/trainer.py
@@ -0,0 +1,19 @@
+class Trainer:
+    ID = 1
+
+    def __init__(self, name):
+        self.name = name
+        self.id = Trainer.generate_id() - 1
+
+    @staticmethod
+    def generate_id():
+        Trainer.ID += 1
+        return Trainer.ID
+
+    @staticmethod
+    def get_next_id():
+        return Trainer.ID
+
+    def __repr__(self):
+        return f"Trainer <{self.id}> {self.name}"
+    
