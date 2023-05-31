d7880530d0abe0e75dc1fa7d02f9dad914a8dae5
Georgi-A-Andreev <g.andreev9@gmail.com>
2023-05-31 13:55:54 +0300

    Calculate Rectangle Area

---
+++ softuni_courses/python_fundamentals/second_time_problems/functions_lab/calculate_rectangle_area.py
@@ -0,0 +1,8 @@
+def calculate_rectangle_area(width, height):
+    return width * height
+
+
+rec_width = int(input())
+rec_height = int(input())
+
+print(calculate_rectangle_area(rec_width, rec_height))
