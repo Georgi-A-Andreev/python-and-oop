90c4bedfccf6c37ee343c5090c315eb6b7576969
Georgi-A-Andreev <g.andreev9@gmail.com>
2023-06-07 14:57:57 +0300

    Softuni reception

---
+++ softuni_courses/python_fundamentals/second_time_problems/mid_exam_prep/mid_exam_prep_2/softuni_reception.py
@@ -0,0 +1,24 @@
+
+one = int(input())
+two = int(input())
+three = int(input())
+
+students = int(input())
+
+together = one + two + three
+counter = 0
+
+while True:
+    if students <= 0:
+        break
+    counter += 1
+    students -= together
+
+    if counter % 4 == 0:
+        counter += 1
+
+
+
+
+print(f'Time needed: {counter}h.')
+
