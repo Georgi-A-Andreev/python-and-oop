9c9cf9e7f2e281b85fbff7eefc07423676383ce0
Georgi-A-Andreev <g.andreev9@gmail.com>
2023-07-03 22:21:54 +0300

    practice recursion

---
+++ softuni_courses/algorithms_with_python/practice/connected_areas.py
@@ -0,0 +1,35 @@
+rows = int(input())
+cols = int(input())
+areas = {}
+matrix = []
+for _ in range(rows):
+    matrix.append(list(input()))
+
+
+def find_all_areas(matrix, row, col):
+    if row < 0 or col < 0 or row >= len(matrix) or col >= len(matrix[0]):
+        return 0
+    if matrix[row][col] != '-':
+        return 0
+    result = 1
+    matrix[row][col] = 'v'
+    result += find_all_areas(matrix, row + 1, col)
+    result += find_all_areas(matrix, row - 1, col)
+    result += find_all_areas(matrix, row, col + 1)
+    result += find_all_areas(matrix, row, col - 1)
+
+    return result
+
+
+for row in range(rows):
+    for col in range(cols):
+        result = find_all_areas(matrix, row, col)
+
+        if result != 0:
+            areas[(row, col)] = result
+
+print(f'Total areas found: {len(areas)}')
+counter = 1
+for k, v in sorted(areas.items(), key=lambda x: -x[1]):
+    print(f'Area #{counter} at {k}, size: {v}')
+    counter += 1
