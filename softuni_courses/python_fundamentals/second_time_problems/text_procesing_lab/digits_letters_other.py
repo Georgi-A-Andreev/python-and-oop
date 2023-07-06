f1be136f68785e37f0e31c1353dad6a28f1804c7
Georgi-A-Andreev <g.andreev9@gmail.com>
2023-07-06 15:57:25 +0300

    digits letter or other

---
+++ softuni_courses/python_fundamentals/second_time_problems/text_procesing_lab/digits_letters_other.py
@@ -0,0 +1,5 @@
+text = input()
+
+print("".join([i for i in text if i.isdigit()]))
+print("".join([i for i in text if i.isalpha()]))
+print("".join([i for i in text if not i.isalpha() and not i.isdigit()]))
