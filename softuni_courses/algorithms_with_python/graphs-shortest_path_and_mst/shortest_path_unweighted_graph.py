a40a487afd0010057df5d58e72d5675e35a5ac1d
Georgi-A-Andreev <g.andreev9@gmail.com>
2023-07-10 20:05:56 +0300

    shortest path in unweigthed graph

---
+++ softuni_courses/algorithms_with_python/graphs-shortest_path_and_mst/shortest_path_unweighted_graph.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+nodes_c = int(input())
+edges_c = int(input())
+
+nodes = []
+[nodes.append([]) for _ in range(nodes_c + 1)]
+edges = []
+
+for _ in range(edges_c):
+    s, d = [int(x) for x in input().split()]
+
+    nodes[s].append(d)
+
+start = int(input())
+destination = int(input())
+
+visited = [False] * (nodes_c + 1)
+parent = [None] * (nodes_c + 1)
+
+visited[start] = True
+queue = deque([start])
+
+while queue:
+    node = queue.popleft()
+    if node == destination:
+        break
+
+    for child in nodes[node]:
+        if visited[child]:
+            continue
+        visited[child] = True
+        queue.append(child)
+        parent[child] = node
+
+result = deque([destination])
+node = destination
+while parent[node] is not None:
+    node = parent[node]
+    result.appendleft(node)
+
+
+print(f'Shortest path length is: {len(result) - 1}')
+print(*result, sep=' ')
