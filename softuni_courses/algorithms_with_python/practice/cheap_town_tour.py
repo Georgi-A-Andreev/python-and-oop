1f7bbaf11053d68cb0d03b1ff5483938cce3cbb5
Georgi-A-Andreev <g.andreev9@gmail.com>
2023-07-16 12:15:47 +0300

    practice mst

---
+++ softuni_courses/algorithms_with_python/practice/cheap_town_tour.py
@@ -0,0 +1,41 @@
+class Edge:
+    def __init__(self, first, second, weight):
+        self.first = first
+        self.second = second
+        self.weight = weight
+
+    def __gt__(self, other):
+        return self.weight > other.weight
+
+
+nodes = int(input())
+edges = int(input())
+
+graph = []
+
+for _ in range(edges):
+    first, second, weight = [int(x) for x in input().split(' - ')]
+    edge = Edge(first, second, weight)
+    graph.append(edge)
+
+parent = [x for x in range(nodes)]
+total_cost = 0
+
+
+def find_root(parent, node):
+    while node != parent[node]:
+        node = parent[node]
+    return node
+
+
+for edge in sorted(graph, key=lambda x: x.weight):
+    first_node_root = find_root(parent, edge.first)
+    second_node_root = find_root(parent, edge.second)
+
+    if first_node_root == second_node_root:
+        continue
+
+    parent[first_node_root] = second_node_root
+    total_cost += edge.weight
+
+print(f'Total cost: {total_cost}')
