cbca382784da6978e87fd910a0ad6ce638958a67
Georgi-A-Andreev <g.andreev9@gmail.com>
2023-07-17 19:03:29 +0300

    practice Dynamic Programming

---
+++ softuni_courses/algorithms_with_python/dynamic_programming_lab/longest_increasing_subsequence.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+n = [int(x) for x in input().split()]
+
+prev = [None] * len(n)
+subsequence = [1] * len(n)
+biggest_so_far = 0
+for idx, el in enumerate(n):
+    for i in range(idx - 1, -1, -1):
+        if el > n[i]:
+            if subsequence[idx] <= subsequence[i] + 1:
+                subsequence[idx] = subsequence[i] + 1
+                prev[idx] = i
+
+for idx, i in enumerate(subsequence):
+    if i > subsequence[biggest_so_far]:
+        biggest_so_far = idx
+
+result = deque()
+
+idx = biggest_so_far
+while idx is not None:
+    result.appendleft(n[idx])
+    idx = prev[idx]
+
+print(*result, sep=' ')
