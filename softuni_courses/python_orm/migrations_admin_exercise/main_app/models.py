3892e28ba5871e9ff7a26cd18ca7b75f68482248
Georgi-A-Andreev <g.andreev9@gmail.com>
2023-10-24 11:11:53 +0300

    practice

--- softuni_courses/python_orm/migrations_admin_exercise/main_app/models.py
+++ softuni_courses/python_orm/migrations_admin_exercise/main_app/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Shoe(models.Model):
     brand = models.CharField(max_length=25)
     size = models.PositiveIntegerField()
+
+class UniqueBrands(models.Model):
+    brand_name = models.CharField(max_length=25)
