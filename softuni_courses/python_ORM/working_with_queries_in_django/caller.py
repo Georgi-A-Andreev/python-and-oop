e2e943635c8bc75ed8f9157c9920a59de4f5ff0a
Georgi-A-Andreev <g.andreev9@gmail.com>
2023-10-31 23:10:35 +0200

    practice orm working with queries

--- softuni_courses/python_ORM/working_with_queries_in_django/caller.py
+++ softuni_courses/python_ORM/working_with_queries_in_django/caller.py
@@ -6,7 +6,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 
-from main_app.models import ArtworkGallery, Laptop, ChessPlayer, Meal, Dungeon
+from main_app.models import ArtworkGallery, Laptop, ChessPlayer, Meal, Dungeon, Workout
 
 
 def show_highest_rated_art():
@@ -156,8 +156,34 @@ def set_new_locations():
     Dungeon.objects.filter(recommended_level=75).update(location="Shadowed Abyss")
 
 
+def show_workouts():
+    workouts = Workout.objects.filter(workout_type__in=["Calisthenics", "CrossFit"])
+
+    return "\n".join(str(x) for x in workouts)
 
 
+def get_high_difficulty_cardio_workouts():
+    return Workout.objects.filter(workout_type="Cardio", difficulty="High").order_by("instructor")
+
+
+def set_new_instructors():
+    Workout.objects.filter(workout_type="Cardio").update(instructor="John Smith")
+    Workout.objects.filter(workout_type="Strength").update(instructor="Michael Williams")
+    Workout.objects.filter(workout_type="Yoga").update(instructor="Emily Johnson")
+    Workout.objects.filter(workout_type="CrossFit").update(instructor="Sarah Davis")
+    Workout.objects.filter(workout_type="Calisthenics").update(instructor="Chris Heria")
+
+
+def set_new_duration_times():
+    Workout.objects.filter(instructor="John Smith").update(duration="15 minutes")
+    Workout.objects.filter(instructor="Sarah Davis").update(duration="30 minutes")
+    Workout.objects.filter(instructor="Chris Heria").update(duration="45 minutes")
+    Workout.objects.filter(instructor="Michael Williams").update(duration="1 hour")
+    Workout.objects.filter(instructor="Emily Johnson").update(duration="1 hour and 30 minutes")
+
+
+def delete_workouts():
+    Workout.objects.exclude(workout_type__in=["Strength", "Calisthenics"]).delete()
 
 
 
