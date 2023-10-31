18850b56752ab70aeec5160144b77df04424027b
Georgi-A-Andreev <g.andreev9@gmail.com>
2023-10-31 20:52:49 +0200

    practice orm working with queries

--- softuni_courses/python_ORM/working_with_queries_in_django/caller.py
+++ softuni_courses/python_ORM/working_with_queries_in_django/caller.py
@@ -6,7 +6,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 
-from main_app.models import ArtworkGallery
+from main_app.models import ArtworkGallery, Laptop
 
 
 def show_highest_rated_art():
@@ -21,6 +21,40 @@ def bulk_create_arts(first_art, second_art):
 
 def delete_negative_rated_arts():
     ArtworkGallery.objects.filter(rating__lt=0).delete()
+
+
+def show_the_most_expensive_laptop():
+    most_expensive = Laptop.objects.order_by("-price", "-id").first()
+
+    return f"{most_expensive.brand} is the most expensive laptop available for {most_expensive.price}$!"
+
+
+def bulk_create_laptops(*args):
+    Laptop.objects.bulk_create(args)
+
+
+def update_to_512_GB_storage():
+    Laptop.objects.filter(brand__in=["Asus", "Lenovo"]).update(storage=512)
+
+
+def update_to_16_GB_memory():
+    Laptop.objects.filter(brand__in=["Apple", "Dell", "Acer"]).update(memory=16)
+
+
+def update_operation_systems():
+    Laptop.objects.filter(brand__exact="Asus").update(operation_system="Windows")
+    Laptop.objects.filter(brand__exact="Apple").update(operation_system="MacOS")
+    Laptop.objects.filter(brand__exact="Dell").update(operation_system="Linux")
+    Laptop.objects.filter(brand__exact="Acer").update(operation_system="Linux")
+    Laptop.objects.filter(brand__exact="Lenovo").update(operation_system="Chrome OS")
+
+
+def delete_inexpensive_laptops():
+    Laptop.objects.filter(price__lt=1200).delete()
+
+
+
+
 
 
 
