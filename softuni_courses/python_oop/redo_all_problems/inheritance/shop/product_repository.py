2565d00577ddd25e3f226ec89d0f84a107d33bf0
Georgi-A-Andreev <g.andreev9@gmail.com>
2023-11-06 14:03:38 +0200

    practice oop inheritance

---
+++ softuni_courses/python_oop/redo_all_problems/inheritance/shop/product_repository.py
@@ -0,0 +1,35 @@
+from project.food import Food
+from project.drink import Drink
+from project.product import Product
+
+
+class ProductRepository:
+    def __init__(self):
+        self.products = []
+
+    def add(self, product):
+        self.products.append(product)
+
+    def find(self, product_name):
+        for p in self.products:
+            if p.name == product_name:
+                return p
+
+    def remove(self, product_name):
+        for p in self.products:
+            if p.name == product_name:
+                self.products.remove(p)
+
+    def __repr__(self):
+
+        return '\n'.join(f'{p.name}: {p.quantity}' for p in self.products)
+
+food = Food("apple")
+drink = Drink("water")
+repo = ProductRepository()
+repo.add(food)
+repo.add(drink)
+print(repo.products)
+print(repo.find("water"))
+repo.find("apple").decrease(5)
+print(repo)
