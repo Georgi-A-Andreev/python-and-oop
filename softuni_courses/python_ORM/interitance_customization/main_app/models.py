6827b3dd65e67364fe59526d8e3eb3451277bddc
Georgi-A-Andreev <g.andreev9@gmail.com>
2023-11-10 16:02:43 +0200

    practice orm django

--- softuni_courses/python_ORM/interitance_customization/main_app/models.py
+++ softuni_courses/python_ORM/interitance_customization/main_app/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -49,3 +50,65 @@ class VengeanceDemonHunter(DemonHunter):
 
 class FelbladeDemonHunter(DemonHunter):
     felblade_ability = models.CharField(max_length=100)
+
+
+class UserProfile(models.Model):
+    username = models.CharField(max_length=70, unique=True)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(blank=True, null=True)
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(UserProfile, related_name="sent_messages", on_delete=models.CASCADE)
+    receiver = models.ForeignKey(UserProfile, related_name="received_messages", on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def mark_as_read(self):
+        self.is_read = True
+
+    def mark_as_unread(self):
+        self.is_read = False
+
+    def reply_to_message(self, reply_content, receiver):
+        message = Message.objects.create(sender=self.receiver, receiver=receiver, content=reply_content)
+        message.save()
+        return message
+
+    def forward_message(self, sender, receiver):
+        message = Message.objects.create(sender=sender, receiver=receiver, content=self.content)
+        message.save()
+        return message
+
+
+class StudentIDField(models.PositiveIntegerField):
+    def to_python(self, value):
+        try:
+            return int(value)
+        except ValueError:
+            pass
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    student_id = StudentIDField()
+
+
+class MaskedCreditCardField(models.CharField):
+    def to_python(self, value):
+        if not isinstance(value, str):
+            raise ValidationError("The card number must be a string")
+
+        if not value.isdigit():
+            raise ValidationError("The card number must contain only digits")
+
+        if len(value) != 16:
+            raise ValidationError("The card number must be exactly 16 characters long")
+
+        return f"****-****-****-{value[-4:]}"
+
+
+class CreditCard(models.Model):
+    card_owner = models.CharField(max_length=100)
+    card_number = MaskedCreditCardField(max_length=20)
