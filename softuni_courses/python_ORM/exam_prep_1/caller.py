3d58897c052df4f7136984c6c623bd26e3fbe805
Georgi-A-Andreev <g.andreev9@gmail.com>
2023-11-17 14:25:26 +0200

    practice orm for regular exam

--- softuni_courses/python_ORM/exam_prep_1/caller.py
+++ softuni_courses/python_ORM/exam_prep_1/caller.py
@@ -1,9 +1,45 @@
 import os
 import django
+from django.db.models import Q, Count
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-# Import your models here
-# Create and run your queries within functions
+
+from main_app.models import Director, Actor, Movie
+
+
+def get_directors(search_name=None, search_nationality=None):
+    if search_nationality is None and search_name is None:
+        return ''
+
+    if search_nationality is not None and search_name is not None:
+        directors = Director.objects.filter(Q(full_name__icontains=search_name) | Q(nationality__icontains=search_nationality))
+
+    elif search_nationality is None:
+        directors = Director.objects.filter(full_name__icontains=search_name)
+
+    elif search_nationality is not None:
+        directors = Director.objects.filter(nationality__icontains=search_nationality)
+
+    directors = directors.order_by('full_name')
+
+    if not directors:
+        return ''
+
+    return '\n'.join(
+        f'Director: {d.full_name}, nationality: {d.nationality}, experience: {d.years_of_experience}'
+        for d in directors
+    )
+
+
+def get_top_director():
+    director = Director.objects.get_directors_by_movie_count().first()
+
+    if not director:
+        return ''
+
+    return f'Top Director: {director.full_name}, movies: {director.c_movies}.'
+
+
--- softuni_courses/python_ORM/exam_prep_1/main_app/models.py
+++ softuni_courses/python_ORM/exam_prep_1/main_app/models.py
@@ -1,12 +1,16 @@
 from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
 from django.db import models
 
+from main_app.managers import CustomManager
+
 
 class Director(models.Model):
     full_name = models.CharField(max_length=120, validators=[MinLengthValidator(2)])
     birth_date = models.DateField(default='1900-01-01')
     nationality = models.CharField(max_length=50, default='Unknown')
     years_of_experience = models.SmallIntegerField(default=0, validators=[MinValueValidator(0)])
+
+    objects = CustomManager()
 
 
 class Actor(models.Model):
@@ -33,6 +37,7 @@ class Movie(models.Model):
     is_classic = models.BooleanField(default=False)
     is_awarded = models.BooleanField(default=False)
     last_updated = models.DateTimeField(auto_now=True)
-    director = models.ForeignKey(Director, on_delete=models.CASCADE)
-    starring_actor = models.ForeignKey(Actor, on_delete=models.SET_NULL, blank=True, null=True, related_name='Actors')
+    director = models.ForeignKey(Director, on_delete=models.CASCADE, related_name='director_movie')
+    starring_actor = models.ForeignKey(Actor, on_delete=models.SET_NULL, blank=True, null=True,
+                                       related_name='actor_movie')
     actors = models.ManyToManyField(Actor)
