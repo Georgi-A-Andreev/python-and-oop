a8e497e3125465c646ca11a8c75fe48c84d18534
Georgi-A-Andreev <g.andreev9@gmail.com>
2023-11-23 18:53:52 +0200

    practice for exam

---
+++ softuni_courses/python_ORM/exam_preps_again/exam_prep1/main_app/models.py
@@ -0,0 +1,37 @@
+from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
+from django.db import models
+
+
+class Director(models.Model):
+    full_name = models.CharField(max_length=120, validators=[MinLengthValidator(2)])
+    birth_date = models.DateField(default='1900-01-01')
+    nationality = models.CharField(max_length=50, default='Unknown')
+    years_of_experience = models.SmallIntegerField(validators=[MinValueValidator(0)], default=0)
+
+
+class Actor(models.Model):
+    full_name = models.CharField(max_length=120, validators=[MinLengthValidator(2)])
+    birth_date = models.DateField(default='1900-01-01')
+    nationality = models.CharField(max_length=50, default='Unknown')
+    is_awarded = models.BooleanField(default=False)
+    last_updated = models.DateTimeField(auto_now=True)
+
+
+class Movie(models.Model):
+    choice = [
+        ('Action', 'Action'),
+        ('Comedy', 'Comedy'),
+        ('Drama', 'Drama'),
+        ('Other', 'Other')
+    ]
+    title = models.CharField(max_length=150, validators=[MinLengthValidator(5)])
+    release_date = models.DateField()
+    storyline = models.TextField(null=True, blank=True)
+    genre = models.CharField(max_length=6, choices=choice, default='Other')
+    rating = models.DecimalField(max_digits=3, decimal_places=1, validators=[MinValueValidator(0), MaxValueValidator(10)], default=0)
+    is_classic = models.BooleanField(default=False)
+    is_awarded = models.BooleanField(default=False)
+    last_updated = models.DateTimeField(auto_now=True)
+    director = models.ForeignKey(Director, on_delete=models.CASCADE, related_name='movies')
+    starring_actor = models.ForeignKey(Actor, on_delete=models.SET_NULL, null=True, blank=True, related_name='movies')
+    actors = models.ManyToManyField(Actor)
