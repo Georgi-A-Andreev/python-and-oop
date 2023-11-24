bcdee5256763e3ff389e26932b42ae0bdb619d1e
Georgi-A-Andreev <g.andreev9@gmail.com>
2023-11-24 21:34:44 +0200

    practice for final exam

--- softuni_courses/python_ORM/exam_preps_again/exam_prep1/caller.py
+++ softuni_courses/python_ORM/exam_preps_again/exam_prep1/caller.py
@@ -5,7 +5,7 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-from django.db.models import Q, Count, Avg
+from django.db.models import Q, Count, Avg, F
 
 from main_app.models import Director, Actor, Movie
 
@@ -54,4 +54,35 @@ def get_top_actor():
     return (f'Top Actor: {actor.full_name}, starring in movies: {movies},'
             f' movies average rating: {actor.avg_rating:.1f}')
 
+
+def get_actors_by_movies_count():
+    actors = Actor.objects.annotate(num_movies=Count('movies_actors')).order_by('-num_movies', 'full_name')[:3]
+
+    if not Movie.objects.all() or not actors:
+        return ''
+
+    return '\n'.join(
+        f'{a.full_name}, participated in {a.num_movies} movies'
+        for a in actors
+    )
+
+
+def get_top_rated_awarded_movie():
+    m = Movie.objects.filter(is_awarded=True).order_by('-rating', 'title').first()
+    if not m:
+        return ''
+    s_actor = m.starring_actor.full_name if m.starring_actor is not None else 'N/A'
+
+    return (f'Top rated awarded movie: {m.title},'
+            f' rating: {m.rating:.1f}. Starring actor: '
+            f'{s_actor}. Cast:'
+            f' {", ".join([a.full_name for a in m.actors.all()])}.')
+
+
+def increase_rating():
+    movies_to_update = Movie.objects.filter(is_classic=True, rating__lt=10)
+    result = movies_to_update.update(rating=F('rating') + 0.1)
+    if result == 0:
+        return f"No ratings increased."
+    return f'Rating increased for {result} movies.'
 
