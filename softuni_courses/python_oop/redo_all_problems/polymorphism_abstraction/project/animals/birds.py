9e0035eb8ba5cf7ea9b3e0d8f03cbbaa940a42d7
Georgi Andreev <129103980+Georgi-A-Andreev@users.noreply.github.com>
2023-11-30 10:30:41 +0200

    practice for final exam

---
+++ softuni_courses/python_oop/redo_all_problems/polymorphism_abstraction/project/animals/birds.py
@@ -0,0 +1,21 @@
+from project.animals.animal import Bird
+from project.food import Meat
+
+
+class Owl(Bird):
+    def make_sound(self):
+        return "Hoot Hoot"
+
+    def feed(self, food):
+        if not isinstance(food, Meat):
+            return f"{self.__class__.__name__} does not eat {food.__class__.__name__}!"
+        self.food_eaten += food.quantity
+        self.weight += 0.25 * food.quantity
+
+class Hen(Bird):
+    def make_sound(self):
+        return "Cluck"
+
+    def feed(self, food):
+        self.food_eaten += food.quantity
+        self.weight += 0.35 * food.quantity
