62e4b37f9d3c725f76155e74423b934ef84b3c75
Georgi-A-Andreev <g.andreev9@gmail.com>
2023-12-15 22:44:45 +0200

    practice web basics

--- softuni_courses/python_web_basics/first_basic_project/calories_counter/models.py
+++ softuni_courses/python_web_basics/first_basic_project/calories_counter/models.py
@@ -3,13 +3,20 @@ from django.db import models
 
 
 class Foods(models.Model):
-    food = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     protein = models.DecimalField(max_digits=4, decimal_places=1, default=0)
     carbs = models.DecimalField(max_digits=4, decimal_places=1, default=0)
     fats = models.DecimalField(max_digits=4, decimal_places=1, default=0)
     total_calories = models.DecimalField(max_digits=7, decimal_places=1, default=0)
 
 
+class Activity(models.Model):
+    name = models.CharField(max_length=100)
+    calories_per_hour = models.IntegerField()
+
+
 class People(models.Model):
     name = models.CharField(max_length=20)
     age = models.IntegerField(validators=[MinValueValidator(0)])
+    total_calories = models.DecimalField(max_digits=10, decimal_places=1, default=0)
+
