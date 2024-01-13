2078b93b5b2d5378b0e6f68d35b91129b01030e0
Georgi-A-Andreev <g.andreev9@gmail.com>
2024-01-13 16:22:17 +0200

    practice django

--- softuni_courses/python_web_basics/second_basic_project/second_basic_project/settings.py
+++ softuni_courses/python_web_basics/second_basic_project/second_basic_project/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'second_basic_project.demo'
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
